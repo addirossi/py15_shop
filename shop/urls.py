127d307cd1474e840f1a580bb61a1144f40651a5
addirossi <addirossi@gmail.com>
2021-12-03 21:33:11 +0600

    Add products list view

--- shop/urls.py
+++ shop/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from product.views import products_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/products/', products_list)
 ]
