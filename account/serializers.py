6ff305fbd4df9fbe897cb37221f7760e2c69606e
addirossi <addirossi@gmail.com>
2021-12-08 21:30:32 +0600

    Add change password

--- account/serializers.py
+++ account/serializers.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model, authenticate
+from django.core.mail import send_mail
+from django.utils.crypto import get_random_string
 from rest_framework import serializers
 
 
@@ -87,8 +89,97 @@ class LoginSerializer(serializers.Serializer):
 
 
 class ChangePasswordSerializer(serializers.Serializer):
-    pass
+    old_pass = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, min_length=6)
+    password_confirm = serializers.CharField(required=True, min_length=6)
+
+    def validate_old_pass(self, old_pass):
+        user = self.context.get('request').user
+        if not user.check_password(old_pass):
+            raise serializers.ValidationError('Неверный пароль')
+        return old_pass
+
+    def validate(self, attrs):
+        pass1 = attrs.get('password')
+        pass2 = attrs.get('password_confirm')
+        if pass1 != pass2:
+            raise serializers.ValidationError('Пароли не совпадают')
+        return attrs
+
+    def set_new_pass(self):
+        user = self.context.get('request').user
+        password = self.validated_data.get('password')
+        user.set_password(password)
+        user.save()
+
+
+# class ForgotPasswordSerializer(serializers.Serializer):
+#     email = serializers.EmailField(required=True)
+#
+#     def validate_email(self, email):
+#         if not User.objects.filter(email=email).exists():
+#             raise serializers.ValidationError('Пользователь не зарегистрирован')
+#         return email
+#
+#     def send_new_pass(self):
+#         email = self.validated_data.get('email')
+#         user = User.objects.get(email=email)
+#         random_pass = get_random_string(15)
+#         user.set_password(random_pass)
+#         user.save()
+#         send_mail('Восстановление пароля',
+#                   f'Ваш новый пароль: {random_pass}',
+#                   'test@gmail.com',
+#                   [email])
 
 
 class ForgotPasswordSerializer(serializers.Serializer):
-    pass
+    email = serializers.EmailField(required=True)
+
+    def validate_email(self, email):
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Пользователь не зарегистрирован')
+        return email
+
+    def send_code(self):
+        email = self.validated_data.get('email')
+        user = User.objects.get(email=email)
+        user.generate_activation_code()
+        send_mail(
+            'Восстановление пароля',
+            f'Ваш код подтверждения: {user.activation_code}',
+            'test@gmail.com',
+            [email]
+        )
+
+
+class ForgotPasswordCompleteSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    code = serializers.CharField(min_length=8, max_length=8, required=True)
+    password = serializers.CharField(required=True)
+    password_confirm = serializers.CharField(required=True)
+
+    def validate_email(self, email):
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Пользователь не зарегистрирован')
+        return email
+
+    def validate_code(self, code):
+        if not User.objects.filter(activation_code=code).exists():
+            raise serializers.ValidationError('Пользователь не зарегистрирован')
+        return code
+
+    def validate(self, attrs):
+        pass1 = attrs.get('password')
+        pass2 = attrs.get('password_confirm')
+        if pass1 != pass2:
+            raise serializers.ValidationError('Пароли не совпадают')
+        return attrs
+
+    def set_new_pass(self):
+        email = self.validated_data.get('email')
+        password = self.validated_data.get('password')
+        user = User.objects.get(email=email)
+        user.set_password(password)
+        user.activation_code = ''
+        user.save()
--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -32,11 +33,20 @@ class LoginView(ObtainAuthToken):
 
 
 class LogoutView(APIView):
-    pass
+    def post(self, request):
+        user = request.user
+        Token.objects.filter(user=user).delete()
+        return Response('Вы успешно разлогинились')
 
 
 class ChangePasswordView(APIView):
-    pass
+    def post(self, request):
+        data = request.data
+        serializer = ChangePasswordSerializer(data=data,
+                                              context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.set_new_pass()
+        return Response('Пароль успешно обновлён')
 
 
 class ForgotPasswordView(APIView):
