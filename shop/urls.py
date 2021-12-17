66fa913f842f2de555e58624e3eade13a0405cfb
addirossi <addirossi@gmail.com>
2021-12-17 21:29:41 +0600

    Add docs

--- shop/urls.py
+++ shop/urls.py
@@ -17,11 +17,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from order.views import CreateOrderView, UsersOrdersList, UpdateOrderStatusView
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Python 15 Shop',
+        default_version='v1',
+        description='Интернет магазин'
+    ),
+    public=True
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/docs/', schema_view.with_ui('swagger')),
     path('api/v1/', include('account.urls')),
     path('api/v1/', include('product.urls')),
     path('api/v1/orders/', CreateOrderView.as_view()),
